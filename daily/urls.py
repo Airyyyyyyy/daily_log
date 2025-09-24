373448012ef43a46c5e4c34b582132e7009b9c4e
Abdulmaleeq123 <Abdulmaleeq.l@kadickintegrated.com>
2025-09-24 13:59:10 +0100

    hosting

--- daily/urls.py
+++ daily/urls.py
@@ -23,8 +23,8 @@ urlpatterns = [
     path('', views.login_view, name='login'),
     path('logs/login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('daily-log/', views.daily_log_view, name='daily_log'),
-    path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
+    path('daily_log/', views.daily_log_view, name='daily_log'),
+    path('admin_dashboard/', views.admin_dashboard, name='admin_dashboard'),
     path('export_logs_excel/', views.export_logs_excel, name='export_logs_excel'),
     path('add_staff/', views.add_staff, name='add_staff'),
 ]
