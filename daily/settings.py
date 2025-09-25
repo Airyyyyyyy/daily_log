7f8454661bf03a83a9d3f311ccdf2b414026e035
Abdulmaleeq123 <Abdulmaleeq.l@kadickintegrated.com>
2025-09-25 11:33:29 +0100

    railway hosting

--- daily/settings.py
+++ daily/settings.py
@@ -129,11 +129,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'logs/static'),  # Path to your app's static folder
-]
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
